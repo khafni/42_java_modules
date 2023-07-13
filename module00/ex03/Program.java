5157ef21dfa69170b85d2ffbcd97dc0f510ced8e
khalil.hafni <khalil.hafni@cgi.com>
2023-07-13 17:35:44 +0100

    module 00 exercice 03 done, TreeMap are really useful to have a sorted map (keys wise)

--- module00/ex03/Program.java
+++ module00/ex03/Program.java
@@ -3,21 +3,48 @@ package module00.ex03;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Program {
+
+    static int getLowestnum(String s) {
+        String[] nums = s.split("\\s+");
+        int min = Integer.MAX_VALUE;
+        for (String num: nums)
+        {
+            if (Integer.valueOf(num) < min)
+                min = Integer.valueOf(num);
+        }
+        return min;
+    }
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        String whichWeek;
         int numberOfMonths = 18;
-        Map<Integer, Integer> weeksGrades = new HashMap<>();
+        String errMsg = "IllegalArgument";
+        Map<Integer, Integer> weeksGrades = new TreeMap<>();
 
         for (int i = 1; i <= numberOfMonths; i++) {
-            whichWeek = scanner.nextLine();
-            if (whichWeek.equals("Week " + String.valueOf(i)))
+            String input = scanner.nextLine();
+            if (input.equals("42"))
+                break;
+            if (input.equals("Week " + String.valueOf(i)) == false)
             {
-                
+                System.err.println(errMsg);
+                System.exit(1);
             }
+            input = scanner.nextLine();
+            int minGrade =  getLowestnum(input);
+            weeksGrades.put(i, minGrade);
+        }
+
+        for (Integer key: weeksGrades.keySet())
+        {
+            System.out.printf("Week %d ", key);
+            int equals = weeksGrades.get(key);
+            for (int i = 0; i < equals; i++)
+                System.out.printf("=");
+            System.out.printf(">\n");
         }
         scanner.close();
     }
