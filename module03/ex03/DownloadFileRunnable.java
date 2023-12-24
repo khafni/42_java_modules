4ebcf899c2e96c918329d32f85519e016be0eac2
khalil <hafnix@gmail.com>
2023-12-24 23:00:08 +0100

    ok

--- module03/ex03/DownloadFileRunnable.java
+++ module03/ex03/DownloadFileRunnable.java
@@ -1,10 +1,50 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class DownloadFileRunnable implements Runnable{
     int number;
-    public DownloadFileRunnable (int number) {
+    String urlString;
+    public DownloadFileRunnable (int number, String urlString) {
        this.number = number;
+       this.urlString = urlString;
     }
+
+//    private
     @Override
     public void run() {
-        System.out.println("number " + 1);
+        URL url = null;
+        HttpURLConnection httpURLConnection = null;
+        try {
+            url = new URL(urlString);
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            int responseCode = httpURLConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_MOVED_PERM || responseCode == HttpURLConnection.HTTP_MOVED_TEMP) {
+                url = new URL(httpURLConnection.getHeaderField("Location"));
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        try (BufferedInputStream bufferedInputStream = new BufferedInputStream(httpURLConnection.getInputStream());
+        ) {
+            System.out.println(Thread.currentThread().getName() + " start download file number " + number);
+            String fileName = new URL(urlString).getPath();
+            fileName = fileName.substring(fileName.lastIndexOf('/') + 1);
+            FileOutputStream fos = new FileOutputStream("./" + fileName);
+            byte[] buffer = new byte[40000];
+            int bytesRead;
+
+            while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
+                fos.write(buffer, 0, bytesRead);
+            }
+        }
+        catch (IOException  e) {
+            System.err.println("url: " + urlString + " is invalid");
+        }
+        System.out.println(Thread.currentThread().getName() + " finish download file number " + number);
     }
 }
--- module03/ex03/Program.java
+++ module03/ex03/Program.java
@@ -12,18 +12,26 @@ public class Program {
     public static void main (String[] args) {
         threadCount = Integer.parseInt(args[0].split("=")[1]);
         ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+//        for (int i = 0; i < threadCount; i++) {
+//            executorService.execute(new DownloadFileRunnable(i));
+//        }
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("files_urls.txt")))
         {
             String line;
+            int i = 0;
             while ((line = bufferedReader.readLine()) != null)
             {
-               System.out.println(line);
+//               System.out.println(line);
+               executorService.execute(new DownloadFileRunnable(i, line));
+               i++;
             }
         }
         catch (IOException e)
         {
-            System.err.println(e.getMessage());
+            System.err.println(e.getLocalizedMessage());
         }
+        executorService.shutdown();
+
 
     }
 }
